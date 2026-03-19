d45ccb1e2f50d03f5139537e3c2792afcd5e8c5b
octavio wina <botkakashi234@gmail.com>
2026-03-19 22:54:09 +0100

    Create page.js

---
+++ app/login/page.js
@@ -0,0 +1,12 @@
+export default function Login() {
+  return (
+    <div className="container">
+      <div className="card">
+        <h2>Connexion</h2>
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Mot de passe" />
+        <button>Se connecter</button>
+      </div>
+    </div>
+  );
+    }

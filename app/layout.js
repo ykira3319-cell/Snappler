b84f08f769968d425552be479b9adb3e9df391ae
ykira3319-cell <ykira3319@gmail.com>
2026-03-22 05:37:53 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,9 @@
 import "../styles/globals.css";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
-      <body>
-        <Navbar />
-        {children}
-        <Footer />
-      </body>
+      <body>{children}</body>
     </html>
   );
 }

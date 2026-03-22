ca15cd4b234100384c42a2c02fa5f6b83dee8f66
ykira3319-cell <ykira3319@gmail.com>
2026-03-22 05:52:14 +0100

    Create schema.sql

---
+++ database/schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE snappler;
+
+USE snappler;
+
+CREATE TABLE users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(100),
+  email VARCHAR(100) UNIQUE,
+  password VARCHAR(255)
+);
